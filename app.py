76f4bd7e9f50b20a0a6be0ce906c4e498b8e0863
Anthony Alvarado <anthonydalvarado@gmail.com>
2023-08-01 19:34:04 -0400

    dynamic data using templates

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+RNGINFO = [
+    {
+        'id':1,
+        'title': 'Basketball'
+    },
+    {
+        'id':2,
+        'title': 'Whiskey',
+        'type': 'Dog'
+    },
+    {
+        'id':3,
+        'title': 'Toyota 86',
+        'price': '$20,000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', rnginfo=RNGINFO)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
